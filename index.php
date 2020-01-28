85368e0d0dcdf491a9befd5d68aed1dd16b4f76a
abidhussain <ah688@njit.edu>
2020-01-28 16:53:38 -0500

    k

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> "Pay page" </title>
+    <link rel="stylesheet" href="style.css">
+
+
+
+</head>
+<body>
+<script src="https://js.stripe.com/v3/"></script>
+
+
+<form action="charge" method="post" id="payment-form">
+    <div class="form-row">
+        <label for="card-element">
+            Credit or debit card
+        </label>
+        <div id="card-element">
+            <!-- A Stripe Element will be inserted here. -->
+        </div>
+
+        <!-- Used to display form errors. -->
+        <div id="card-errors" role="alert"></div>
+    </div>
+
+    <button>Submit Payment</button>
+</form>
